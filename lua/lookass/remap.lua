dd2f819114d93797bbc3f0630ddebd3864eafd85
lukaslinardi <lukaslinardi@gmail.com>
2024-04-22 21:57:52 +0700

    chore: add faster command for split window

--- lua/lookass/remap.lua
+++ lua/lookass/remap.lua
@@ -3,4 +3,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<C-r>", vim.cmd.redo)
-
+vim.keymap.set("n", "<leader>gg", ":Gvdiff<CR>")
+vim.keymap.set("n", "<leader>vv", ":vsplit<CR>")
+vim.keymap.set("n", "<leader>hh", ":split<CR>")
