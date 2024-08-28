70375ce70096735fdb3cd4bb13907cee7823ee1d
Lukas <lukaslinardi@gmail.com>
2024-08-28 08:46:20 +0700

    chore: update conform

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -4,14 +4,16 @@ conform.setup({
     formatters_by_ft = {
         lua = { "stylua" },
         python = { "black" },
-        javascript = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
-        javascriptreact = { { "prettierd", "prettier" } },
-        typescriptreact = { { "prettierd", "prettier" } },
-        go = { { "goimports", "gofmt", "gopls" } },
-    }
+        javascript = { "prettierd", "prettier" }, -- Remove nested braces and apply stop_after_first
+        typescript = { "prettierd", "prettier" },
+        javascriptreact = { "prettierd", "prettier" },
+        typescriptreact = { "prettierd", "prettier" },
+        go = { "goimports", "gofmt", "gopls" },
+    },
+    stop_after_first = true, -- Ensure this is applied correctly
 })
 
+-- Keybinding for formatting
 vim.keymap.set({ "n", "v" }, "F", function()
     conform.format({ lsp_fallback = true, async = false, timeout_ms = 500 })
 end)
